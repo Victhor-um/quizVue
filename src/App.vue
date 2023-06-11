1b064ff689c3b645d4cf5d12c2b19d17cb2a3f5b
Victor <uberman59@gmail.com>
2023-06-11 19:55:04 +0500

    feat: make components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div class="ctr">
-    <div class="questions-ctr">
-      <div class="progress">
-        <div class="bar"></div>
-        <div class="status">1 out of 3 questions answered</div>
-      </div>
-      <div class="single-question">
-        <div class="question">Sample Question 1</div>
-        <div class="answers">
-          <div class="answer">Sample Answer 1</div>
-          <div class="answer">Sample Answer 2</div>
-          <div class="answer">Sample Answer 3</div>
-          <div class="answer">Sample Answer 4</div>
-        </div>
-      </div>
-    </div>
-    <div class="result">
-      <div class="title">You got sample result 1!</div>
-      <div class="desc">Enter a short description here about the result.</div>
-    </div>
+    <quizQuestions
+      v-if="questionsAnswered < data.questions.length"
+      :questions="data.questions"
+    />
+    <quizResult v-else />
     <button type="button" class="reset-btn">Reset</button>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import quizQuestions from '@/components/quizQuestions.vue';
+import quizResult from '@/components/quizResult.vue';
+import { ref } from 'vue';
+import { data } from '@/data.js';
+
+const questionsAnswered = ref(0);
+</script>
 
 <style lang="scss" scoped></style>
